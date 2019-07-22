e4c63ddbdbad6be330daa73214e8a9cef38427fb
VPpexis <vpp5010207@gmail.com>
2019-07-22 11:52:15 +0800

    07/22/19 11:50 AM
    -Added Dynamic Link Lib
    -Added scoreList class
    -Added STL Feature

---
+++ dependencies/ConsoleChecker/ConsoleChecker/ConsoleChecker.cpp
@@ -0,0 +1,12 @@
+// ConsoleChecker.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "playerList.h"
+
+int main()
+{
+	playerList p;
+	int x = p.init();
+	std::cout << x << std::endl;;
+}
